20f41de5c1352ccb3d3fd6f46d1fb6f43b032b5f
edgardo9 <e.llanos1989@gmail.com>
2024-03-10 02:32:49 -0300

    Principalmente se agregan recursos bootstrap locales, se agrega seccion partners y se agrega configuracion router angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { IvantiComponent } from './partners/ivanti/ivanti.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: "home", 
+        component: HomeComponent
+    },
+    { 
+        path: "ivanti", 
+        component: IvantiComponent
+    },
+    { 
+        path: '**', 
+        redirectTo: 'home'
+    }
+];
---
+++ src/app/partners/ivanti/ivanti.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-ivanti',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './ivanti.component.html',
+  styleUrl: './ivanti.component.css'
+})
+export class IvantiComponent {
+
+}
